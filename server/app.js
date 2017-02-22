81fc7c0e9a7f259a9af0e2fa8b3030075e64514e
Aurélien Demilly <demilly.aurelien@gmail.com>
2017-02-22 17:04:57 +0100

    fix pathing issue

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(
 app.use(express.static(path.resolve(__dirname, '..', 'dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'));
 });
 
 export default app;
